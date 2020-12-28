026f0ca1e073db2bf5588e02ce3fff773c8b1df3
kim-misol <wonderfulpine@gmail.com>
2020-12-28 16:23:38 +0900

    Add comments about the flow

--- ch08/8.1 CNN을 활용한 캔들차트 예측분석/myDeepCNN.py
+++ ch08/8.1 CNN을 활용한 캔들차트 예측분석/myDeepCNN.py
@@ -45,24 +45,31 @@ def build_dataset(data_directory, img_width):
     return feature, label, nb_classes
 
 
+# 모델 구조
 def build_model(SHAPE, nb_classes, bn_axis, seed=None):
 
     if seed:
+        # 예측 result를 재현하기 위해 특정 sedd 값을 설정
         np.random.seed(seed)
-
+    # 입력값을 전달받는 층을 정의
     input_layer = Input(shape=SHAPE)
 
     # Step 1
+    # 2차원 합성곱층(Conv2D)을 정의, input_layer를 함수형으로 연결
     x = Conv2D(32, 3, 3, init='glorot_uniform',
                border_mode='same', activation='relu')(input_layer)
     # Step 2 - Pooling
+    # Max Pooling Layer를 정의, feature 값 추출을 위해 존재하는 층 (CNN 구성)
     x = MaxPooling2D(pool_size=(2, 2))(x)
 
     # Step 1
+    # Conv2D 추가
     x = Conv2D(48, 3, 3, init='glorot_uniform', border_mode='same',
                activation='relu')(x)
     # Step 2 - Pooling
+    # MaxPooling2D 추가
     x = MaxPooling2D(pool_size=(2, 2))(x)
+    # 신경망 학습시 과적합 방지를 위해 일부 연결층을 제거하는 drop out 레이어를 추가
     x = Dropout(0.25)(x)
 
     # Step 1
@@ -88,7 +95,7 @@ def build_model(SHAPE, nb_classes, bn_axis, seed=None):
     x = Dropout(0.5)(x)
 
     x = Dense(output_dim=2, activation='softmax')(x)
-
+    # 최종 연결된 출력층 (x)과 최초 입력층 (input_layer)을 전달해 모델 구축
     model = Model(input_layer, x)
 
     return model
@@ -133,17 +140,22 @@ def main():
         "{}/test".format(data_directory), args.dimension)
     print("number of classes : {}".format(nb_classes))
 
+    # 모델 생성
     model = build_model(SHAPE, nb_classes, bn_axis)
-
+    # 모델 학습
+    # Adam optimizer를 선언
+    # 범주형 엔트로피 손실 함수 설정
+    # 학습 과정에서 정확도를 관찰한다
     model.compile(optimizer=Adam(lr=1.0e-4),
                   loss='categorical_crossentropy', metrics=['accuracy'])
 
     # Fit the model
+    # 검증 데이터 없이 오로지 학습 데이터로만 학습 수행
     model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs)
 
     # Save Model or creates a HDF5 file
-    model.save('{}epochs_{}batch_cnn_model_{}.h5'.format(
-        epochs, batch_size, data_directory.replace("/", "_")), overwrite=True)
+    # 학습 완료된 모델을 재사용하기 위해 저장
+    model.save(f'{epochs}epochs_{batch_size}batch_cnn_model_{data_directory}.h5'.replace("/", "_"), overwrite=True)
     # del model  # deletes the existing model
     predicted = model.predict(X_test)
     y_pred = np.argmax(predicted, axis=1)
